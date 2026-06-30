499e749ba4cf636e6d6952b9771a1f517203bbcc
nallamsrikanth12 <nallamsrikanth2@gmail.com>
2026-06-30 18:16:30 +0530

    backend-deploy

---
+++ terraform/01-vpc/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+    source = "git::https://github.com/nallamsrikanth2/terraform-infra-dev.git//01-vpc?ref=main"
+    project_name = var.project_name
+    environment =  var.environment
+    common_tags = var.common_tags
+    public_subnets_cidrs = var.public_subnet_cidrs
+    private_subnets_cidrs = var.private_subnet_cidrs
+    database_subnets_cidrs = var.database_subnet_cidrs
+    is_peering_required = var.is_peering_required
+}
